b0101388568919a262e1a34a5e2d2ca1112a9b51
Nikitayende <142479134+Nikitayende@users.noreply.github.com>
2023-08-19 12:23:42 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,9 +1,18 @@
 public class Array{
 public static void main(String[] arg){
 
-String a="sham";
+//String a="sham";
 String name[]={"sham","ram","sita","gita"};
 System.out.println(name[3]);
+System.out.println(name[2]);
+System.out.println(name[1]);
+System.out.println(name[0]);
 
+int number[]={23,49,46,38};
+System.out.println(number[3]);
+
+for(int i=0; i<=number.length; i++){
+ System.out.println(number[i]);
+}
 }
 }
